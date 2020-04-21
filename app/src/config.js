24da1d420c261fa59b37b3e09b73e8e45ff86e94
denny <denny.george90@gmail.com>
2020-04-21 15:29:47 +0530

    (feat) stable docker-compose up

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,5 @@
 // development
-// export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
+export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
 
-// production
-export const ARCHIVE_SERVER_PATH = 'https://archive-server.tattle.co.in';
+// // production
+// export const ARCHIVE_SERVER_PATH = 'https://archive-server.tattle.co.in';
