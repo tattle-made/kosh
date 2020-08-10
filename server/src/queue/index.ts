3ed8c042cc6d2ab707b626b310dba267f4558d68
denny <denny.george90@gmail.com>
2020-08-10 14:32:22 +0530

    (chore) wip

--- server/src/queue/index.ts
+++ server/src/queue/index.ts
@@ -1,26 +1,24 @@
-import {Queue as QueueType, Job} from 'bull';
+import { Queue as QueueType, Job } from 'bull';
 import * as Queue from 'bull';
 import { CreateStoryRequestModel } from '../routes/fact-checked-stories/CreateStoryRequestModel';
 // tslint:disable-next-line:no-var-requires
 const { setQueues } = require('@tattle-made/bull-board');
-import {Promise} from 'bluebird';
+import { Promise } from 'bluebird';
 // import { PostCreateRequest } from '../../build/models/request/PostCreateRequest';
 import { PostCreateRequest } from '../models/request/PostCreateRequest';
 import { PostIndexJobCreateModel } from '../routes/posts/PostIndexJobCreateModel';
 import { SearchServer } from '../service/search-server';
 
-
-
 class QueueManager {
     private postIndexQueue: QueueType;
 
     constructor() {
-        this.postIndexQueue = new Queue('Whatsapp Post Index Queue', `redis://${process.env.REDIS_HOST}:6379`);
+        this.postIndexQueue = new Queue(
+            'Whatsapp Post Index Queue',
+            `redis://${process.env.REDIS_HOST}:6379`,
+        );
 
-
-        setQueues([
-            this.postIndexQueue,
-        ]);
+        setQueues([this.postIndexQueue]);
 
         this.setupWorker();
     }
@@ -36,12 +34,19 @@ class QueueManager {
     public setupWorker() {
         this.postIndexQueue.process((job) => {
             const searchServer = new SearchServer();
-            return searchServer.indexPostLoose(job.toJSON().data)
+            return searchServer
+                .indexPostLoose(job.toJSON().data)
                 .then()
                 .catch((err) => {
                     this.postIndexQueue.pause();
                     // tslint:disable-next-line:max-line-length
-                    return Promise.reject(new Error(err.response.status + '\n' + err.response.statusText));
+                    return Promise.reject(
+                        new Error(
+                            err.response.status +
+                                '\n' +
+                                err.response.statusText,
+                        ),
+                    );
                 });
         });
 
@@ -57,6 +62,6 @@ class QueueManager {
     }
 }
 
-const queueManagerInstance =  new QueueManager();
+const queueManagerInstance = new QueueManager();
 
 export default queueManagerInstance;
